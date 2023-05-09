8cb0cbfe191746bb18d025608e8ccde04e963cd8
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-05-09 23:37:11 +0530

    Time: 60 ms (44.72%) | Memory: 42 MB (60.17%) - LeetSync

--- filter-elements-from-array/filter-elements-from-array.js
+++ filter-elements-from-array/filter-elements-from-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn);
+
+    const res = [];
+    for(let i = 0;i<arr.length;i++){
+        if(fn(arr[i],i))
+            res.push(arr[i]);
+    }
+    return res;
 };
