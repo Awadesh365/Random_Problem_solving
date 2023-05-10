5e0667fa41fea2edd743c0708d47564a34a85c09
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-05-10 20:50:23 +0530

    Time: 65 ms (37.09%) | Memory: 42.3 MB (68.42%) - LeetSync

--- array-reduce-transformation/array-reduce-transformation.js
+++ array-reduce-transformation/array-reduce-transformation.js
@@ -6,9 +6,9 @@
  */
  // POS Js
 var reduce = function(nums, fn, init) {
-    let accumulator = init;
-    for(const element of nums){
-        accumulator = fn(accumulator,element);
-    }
-    return accumulator;
+   let accumulator = init;
+   nums.forEach((element)=>{
+       accumulator = fn(accumulator,element);
+   });
+   return accumulator;
 };
