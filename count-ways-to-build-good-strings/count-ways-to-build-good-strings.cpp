266b5c756fb00296054e7ad17c634ccfb0d3f6d9
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-05-13 12:36:04 +0530

    Time: 20 ms (42.12%) | Memory: 12.5 MB (53.30%) - LeetSync

--- count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,37 +1,40 @@
 class Solution
 {
-    vector<int> dp;
-    int mod = (1e9 + 7);
+    vector<int> dp; // Vector to store the results of subproblems
+    int mod = (1e9 + 7); // Modulus value for the final answer
 
-    int dfs(int end, int zero, int one)
+    int dfs(int end, int zero, int one) // Helper function to count the number of good strings of length "end"
     {
-        if (dp[end] != -1)
+        if (dp[end] != -1) // If we have already computed the result for this subproblem, return it.
             return dp[end];
 
-        int cnt = 0;
+        int cnt = 0; // Counter variable to store the number of good strings of length "end"
+
+        // If we can add "one" to the end of the string, we add it and recursively count the number of good strings for the remaining length
         if (end >= one)
             cnt += dfs(end - one, zero, one);
 
+        // If we can add "zero" to the end of the string, we add it and recursively count the number of good strings for the remaining length
         if (end >= zero)
             cnt += dfs(end - zero, zero, one);
 
-        dp[end] = cnt % mod;
-        return dp[end];
+        dp[end] = cnt % mod; // Store the result for this subproblem to avoid recomputing it later
+        return dp[end]; // Return the result
     }
 
 public:
     int countGoodStrings(int low, int high, int zero, int one)
     {
-        dp.resize(high + 1, -1);
-        dp[0] = 1;
+        dp.resize(high + 1, -1); // Initialize the "dp" vector with -1
+        dp[0] = 1; // There is only one way to form a string of length zero, which is the empty string.
 
-        int ans = 0;
-        for (int end = low; end <= high; ++end)
+        int ans = 0; // Variable to store the final answer
+        for (int end = low; end <= high; ++end) // Loop through all possible lengths between "low" and "high"
         {
-            ans += dfs(end, zero, one);
-            ans %= mod;
+            ans += dfs(end, zero, one); // Count the number of good strings for length "end" and add it to the final answer
+            ans %= mod; // Take the modulus to avoid integer overflow
         }
 
-        return ans;
+        return ans; // Return the final answer
     }
 };
