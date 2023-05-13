a455611ba977d01401ea21fe6a6902f2c27772cf
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-05-13 11:55:41 +0530

    Time: 20 ms (42.12%) | Memory: 12.4 MB (59.89%) - LeetSync

--- count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,27 +1,37 @@
-class Solution {
+class Solution
+{
+    vector<int> dp;
+    int mod = (1e9 + 7);
+
+    int dfs(int end, int zero, int one)
+    {
+        if (dp[end] != -1)
+            return dp[end];
+
+        int cnt = 0;
+        if (end >= one)
+            cnt += dfs(end - one, zero, one);
+
+        if (end >= zero)
+            cnt += dfs(end - zero, zero, one);
+
+        dp[end] = cnt % mod;
+        return dp[end];
+    }
+
 public:
-// POD
-    const int mod = 1000000007;
-    
-    int countGoodStrings(int low, int high, int zero, int one) {
-        int score = 0;
-        vector<int> dp(high + 1, 0);
+    int countGoodStrings(int low, int high, int zero, int one)
+    {
+        dp.resize(high + 1, -1);
         dp[0] = 1;
-        for (int k = 1; k <= high; k++) {
-            if (k >= zero) {
-                dp[k] += dp[k - zero];
-                dp[k] %= mod;
-            }
-            if (k >= one) {
-                dp[k] += dp[k - one];
-                dp[k] %= mod;
-            }
+
+        int ans = 0;
+        for (int end = low; end <= high; ++end)
+        {
+            ans += dfs(end, zero, one);
+            ans %= mod;
         }
-        
-        for (int k = low; k <= high; k++) {
-            score += dp[k];
-            score %= mod;
-        }
-        return score;
+
+        return ans;
     }
 };
