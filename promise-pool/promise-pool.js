8d979734de586e393ab5b3dfe1eedd5b6c24ed01
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-05-17 11:32:07 +0530

    Time: 62 ms (62.94%) | Memory: 42.1 MB (63.54%) - LeetSync

--- promise-pool/promise-pool.js
+++ promise-pool/promise-pool.js
@@ -6,29 +6,18 @@
  //POD js
  // about promise pool in js
 
- // approach 1:- recusive helper function
-var promisePool = async function(functions, n) {
-    return new Promise((resolve)=>{
-        let inProgressCount = 0;
-        let functionIndex =0;
-        function helper(){
-            if(functionIndex>=functions.length){
-                if(inProgressCount===0) resolve();
+ // approach 2:- Async/Await + Promise.all() + Array.shift()
 
-                return;
-            }
-            while(inProgressCount<n && functionIndex < functions.length){
-                inProgressCount++;
-                const promise = functions[functionIndex]();
-                functionIndex++;
-                 promise.then(() =>{
-                    inProgressCount--;
-                    helper();
-                });
-            }
-        }
-        helper();
-    });
+var promisePool = async function(functions, n) {
+    async function evaluateNext(){
+        if(functions.length===0)    return;
+
+        const fn = functions.shift();
+        await fn();
+        await evaluateNext();
+    }
+    const nPromises = Array(n).fill().map(evaluateNext);
+    await Promise.all(nPromises);
 };
 
 /**
