8d0d022f95b3a67fc39b70af7859fc39ef87d904
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-05-27 21:39:10 +0530

    Time: 28 ms (50.00%) | Memory: 47.8 MB (12.50%) - LeetSync

---
+++ maximum-strength-of-a-group/maximum-strength-of-a-group.cpp
@@ -0,0 +1,64 @@
+class Solution
+{
+public:
+    typedef long long ll;
+    ll maxStrength(vector<int> &nums)
+    {
+        if (nums.size() == 1)
+            return nums[0];
+
+        vector<int> pos, neg;
+        for (auto &it : nums)
+        {
+            if (it > 0)
+                pos.push_back(it);
+            else if (it < 0)
+                neg.push_back(it);
+        }
+
+        ll prod = (ll)1;
+        ll psedoProd = (ll)1;
+        bool check = false;
+        for (auto &it : nums)
+        {
+            if (it != 0)
+                prod *= (ll)it;
+            else
+            {
+                check = true;
+                psedoProd *= it;
+            }
+        }
+
+        set<int> chk;
+        for (auto &it : nums)
+            chk.insert(it);
+
+        if (chk.size() == 1)
+        {
+            for (auto &it : chk)
+                if (it == 0)
+                    return 0;
+        }
+
+        if (neg.size() & 1)
+        {
+            if (neg.size() == 1)
+            {
+                if (chk.size() == 2)
+                {
+                    for (auto &it : chk)
+                        if (it == 0)
+                            return 0;
+                }
+            }
+            
+            sort(neg.begin(), neg.end(), greater<int>());
+            prod = (prod / neg[0]);
+
+            return prod;
+        }
+
+        return prod;
+    }
+};
