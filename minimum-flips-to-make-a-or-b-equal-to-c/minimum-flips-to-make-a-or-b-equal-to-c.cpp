8447c5008b23675e1b5deb4c654f7115e1de1674
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-06-07 09:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (53.28%) - LeetSync

--- minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -3,6 +3,7 @@
 class Solution
 {
 public:
+    // __popcount works only in c++20 and onwards
     int minFlips(int a, int b, int c)
     {
         return __builtin_popcount((a | b) ^ c) + __builtin_popcount(a & b & ((a | b) ^ c));
