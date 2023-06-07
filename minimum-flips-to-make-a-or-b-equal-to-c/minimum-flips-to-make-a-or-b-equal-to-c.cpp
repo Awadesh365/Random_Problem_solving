f6320cd88e6158f8e29303c15a25557465d6371f
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-06-07 09:22:41 +0530

    Time: 3 ms (40.05%) | Memory: 6 MB (53.28%) - LeetSync

---
+++ minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,10 @@
+// NM...POD
+// FS_..B...EXMS
+class Solution
+{
+public:
+    int minFlips(int a, int b, int c)
+    {
+        return __builtin_popcount((a | b) ^ c) + __builtin_popcount(a & b & ((a | b) ^ c));
+    }
+};
