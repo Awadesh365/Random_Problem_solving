d3fd2c869ced7d1a0205365532bc9724ade7e2c0
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-06-09 09:53:13 +0530

    Time: 18 ms (47.81%) | Memory: 15.9 MB (72.43%) - LeetSync

--- find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-// approach 1: - brute force
-// O(n)
+// binSrch O(log(n))
+// NM...POD
+// FS_..B...EXMS
+
 char nextGreatestLetter(vector<char> &letters, char target)
 {
+    int n = letters.size();
     char ans = letters[0];
-   for(auto &it:letters){
-       if(it>target){
-           ans = it;
-           break;
-       }
-   }
+    int left = 0, right = (n-1);
+
+    if(ans>target)
+        return ans;
+
+    if(target>= letters[n-1])
+        return ans;
+
+    while(left<=right){
+        int mid = (right+left)/2;
+
+        if(letters[mid]>target){
+            ans = letters[mid];
+            right = mid-1;
+        }
+        else{
+            left = mid+1;
+        }
+    }
    return ans;
 }
 };
