a4685b2f010b33de26fcfe241dc81c41467c2448
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-06-11 10:45:46 +0530

    Time: 398 ms (60.35%) | Memory: 145.7 MB (43.71%) - LeetSync

---
+++ snapshot-array/snapshot-array.cpp
@@ -0,0 +1,39 @@
+class SnapshotArray {
+public:
+    // NM... IDK
+    // POD, this was really hard to get and 
+    // understand that what exectly i do,
+
+        int snapId;
+    vector<vector<pair<int, int>>> historyRecords;
+    SnapshotArray(int length) {
+        snapId = 0;
+        historyRecords.resize(length);
+        for (int i = 0; i < length; ++i) {
+            historyRecords[i].push_back(make_pair(0, 0));
+        }
+    }
+    
+    void set(int index, int val) {
+        historyRecords[index].push_back(make_pair(snapId, val));
+    }
+    
+    int snap() {
+        return snapId++;
+    }
+
+    // NOTE:- Giving wrong ans when i am using {,} instead of makepair, and also when i am using .second instead of ->second
+
+    int get(int index, int snap_id) {
+        auto it = upper_bound(historyRecords[index].begin(), historyRecords[index].end(), make_pair(snap_id, INT_MAX));
+        return prev(it)->second;
+    }
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
+ * obj->set(index,val);
+ * int param_2 = obj->snap();
+ * int param_3 = obj->get(index,snap_id);
+ */
