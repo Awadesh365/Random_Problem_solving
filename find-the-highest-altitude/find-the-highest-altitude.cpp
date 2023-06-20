01a026e22748202f20fb34cbe8e8bcda63e16f0e
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-06-20 06:38:23 +0530

    Time: 6 ms (28.47%) | Memory: 8 MB (54.25%) - LeetSync

---
+++ find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+// mrg brs -> fk sm -> NM
+    int largestAltitude(vector<int>& gain) {
+        int currentAltitude = 0;
+        // Highest altitude currently is 0.
+        int highestPoint = currentAltitude;
+        
+        for (int &altitudeGain : gain) {
+            // Adding the gain in altitude to the current altitude.
+            currentAltitude += altitudeGain;
+            // Update the highest altitude.
+            highestPoint = max(highestPoint, currentAltitude);
+        }
+        
+        return highestPoint;
+    }
+};
