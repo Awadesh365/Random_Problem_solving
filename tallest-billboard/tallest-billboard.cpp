0fee4c38f515cb413096de89bd18bd6f77397bcf
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-06-24 07:37:23 +0530

    Time: 517 ms (19.82%) | Memory: 86.8 MB (22.47%) - LeetSync

---
+++ tallest-billboard/tallest-billboard.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tallestBillboard(vector<int>& rods) {
+        map<int,int>dp;
+        dp[0]=0;
+
+        for(auto &it:rods){
+            map<int,int>temp = dp;
+
+            for(auto &[k,v]:temp){
+                dp[k+it] = max(dp[k+it],v);
+
+                if(it<=k) dp[k-it] = max(dp[k-it],v+it);
+                else dp[it-k] = max(dp[it-k],v+k);
+            }
+        }
+        return dp[0];
+    }
+};
