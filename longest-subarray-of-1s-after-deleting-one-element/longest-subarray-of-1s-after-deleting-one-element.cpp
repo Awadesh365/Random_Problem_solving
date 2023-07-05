e3b7147a3ba7cdc52b63ca15448eb31171120d15
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-07-05 21:08:59 +0530

    Time: 52 ms (28.13%) | Memory: 36.5 MB (19.62%) - LeetSync

--- longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,35 +1,43 @@
-class Solution {
+// #include <bits/stdc++.h>
+// using namespace std;
+
+class Solution
+{
 public:
     int longestSubarray(vector<int> &nums)
-{
-    // like how many zeros you're considering, but at the end that should be maximum only 1 zero can be there
-    int countOfZeros = 0;
-
-    // from starting to ending considring the maximum of 1 zero and maximum count of 1
-    int longestWindow = 0;
-
-    // left most point of the operation space
-    int startingPoint = 0;
-
-    for (int i = 0; i < nums.size(); i++)
     {
-        int it = nums[i];
+        // like how many zeros you're considering, but at the end that should have maximum to maximum only 1 zero.
+        int countOfZeros = 0;
 
-        if (it == 0)
-            countOfZeros++;
+        // from starting to ending considring the maximum of 1 count of zero, and rest of 1's
+        int longestWindow = 0;
 
-        // minimization of zeros in the operation space -> skriking the window
-        // number of zeros should be maximum of count 1.
+        // left most point of the operation space
+        int startingPoint = 0;
 
-        while (countOfZeros >= 2)
+        for (int i = 0; i < nums.size(); i++)
         {
-            if (nums[startingPoint] == 0)
-                countOfZeros--;
+            int it = nums[i];
 
-            startingPoint++;
+            if (it == 0)
+                countOfZeros++;
+
+            // minimization of zeros in the operation space -> skriking the window
+            // number of zeros should be maximum of count 1.
+
+            while (countOfZeros >= 2)
+            {
+                if (nums[startingPoint] == 0)
+                    countOfZeros--;
+
+                startingPoint++;
+            }
+            longestWindow = max(longestWindow, i - startingPoint);
         }
-        longestWindow = max(longestWindow, i - startingPoint);
+        return longestWindow;
     }
-    return longestWindow;
-}
 };
+// int main()
+// {
+//     return 0;
+// }
