8ad7661162dc7febcebcde5cfbc1a0b1b0b44980
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-07-17 22:31:37 +0530

    Time: 3 ms (61.58%) | Memory: 6.1 MB (10.32%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-   int mySqrt(int x)
+
+typedef long long ll;
+int mySqrt(int x)
 {
-    int ans = 1;
-    for (long long i = 0; i<= x; i++)
+    ll left = 0, right = x;
+    while (left <= right)
     {
-        if (i * i <= x)
-            ans = i;
-        else 
-            return i-1;
+        ll mid = left + (right - left) / 2;
+
+        ll check = (mid * mid), test = (mid + 1) * (mid + 1);
+        if (check <= x && test > x)
+            return mid;
+        else
+        {
+            if(check<x)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
     }
-    return ans;
+    return (x - 1);
 }
+
 };
