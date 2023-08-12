1558f59b55c01a0fdf46ef3adeb4246c1e0141b6
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-08-12 23:30:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (61.39%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-   vector<vector<int>> ans;
-void sub(int n, vector<int>&ds,int idx,vector<int>&nums){
-    if(idx >= n){
+void findSubSets(int idx, vector<int> &ds, vector<int> &arr, int n, vector<vector<int>> &ans)
+{
+    if (idx == n)
+    {
         ans.push_back(ds);
         return;
     }
-    ds.push_back(nums[idx]);
-    sub(n, ds, idx + 1,nums);
 
+    ds.push_back(arr[idx]);
+    findSubSets(idx + 1, ds, arr, n, ans);
     ds.pop_back();
-    sub(n, ds, idx + 1,nums);
+    findSubSets(idx + 1, ds, arr, n, ans);
 }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+         vector<int> ds;
+         int n = nums.size();
 
-vector<vector<int>> subsets(vector<int> &nums)
-{
-    vector<int>ds;
-    sub(nums.size(),ds,0,nums);
-    return ans;
-}
+         findSubSets(0, ds,nums, n, ans);
 
+        return ans;
+    }
 };
