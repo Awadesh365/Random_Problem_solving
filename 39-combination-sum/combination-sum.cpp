e8bf59be91be244fb4ebaf2302087573a8ad6390
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-08-13 00:46:55 +0530

    Time: 4 ms (81.80%) | Memory: 10.7 MB (84.36%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,54 @@
-class Solution
-{
+   // #include <bits/stdc++.h>
+// using namespace std;
+class Solution {
 public:
-  void subArray(int idx, vector<int> &ds, int trg, vector<int> &candidates, vector<vector<int>> &ans){
-    if (idx == candidates.size()){
-      if (trg == 0){
+
+void findCombinations(vector<int> &candidates, int target, vector<vector<int>> &ans, vector<int> &ds, int sum, int idx)
+{
+    if (sum > target || idx >= candidates.size())
+        return;
+
+    if (target == sum)
+    {
         ans.push_back(ds);
-      }
-      return;
+        return;
     }
-    if (candidates[idx] <= trg){
-      ds.push_back(candidates[idx]);
-      subArray(idx, ds, trg - candidates[idx], candidates, ans);
-      ds.pop_back();
-    }
-    subArray(idx + 1, ds, trg, candidates, ans);
-  }
-  vector<vector<int>> combinationSum(vector<int> &candidates, int target){
+    sum += candidates[idx];
+    ds.push_back(candidates[idx]);
+    findCombinations(candidates, target, ans, ds, sum, idx);
+
+    sum -= candidates[idx];
+    ds.pop_back();
+    findCombinations(candidates, target, ans, ds, sum, idx + 1);
+}
+vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+{
     vector<vector<int>> ans;
     vector<int> ds;
-    subArray(0, ds, target, candidates, ans);
-
+    findCombinations(candidates, target, ans, ds, 0, 0);
     return ans;
-  }
+}
+
 };
+
+// int main()
+// {
+        // solution s;
+//     int n, target;
+//     cin >> n >> target;
+//     vector<int> arr(n);
+//     for (auto &it : arr)
+//         cin >> it;
+
+//     vector<vector<int>> ans = s.combinationSum(arr, target);
+//     for (auto &it : ans)
+//     {
+//         for (auto &kt : it)
+//         {
+//             cout << kt << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
