d16433a4d5e21efac0525c1d0f5f6b96fe1600f7
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-08-13 23:47:41 +0530

    Time: 4 ms (54.48%) | Memory: 16.5 MB (21.27%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-   void generateSubSets(vector<int> &nums, set<vector<int>> &powerSet, vector<int> subSet, int index)
+   void generateSubSets(vector<int> &nums, vector<vector<int>> &powerSet, vector<int> subSet, int index)
 {
     if (index >= nums.size())
     {
-        sort(subSet.begin(), subSet.end());
-        powerSet.insert(subSet); // that set is has exceed the size of array.
+        powerSet.push_back(subSet); // that set is has exceed the size of array.
         return;
     }
 
     subSet.push_back(nums[index]);                      // we are considering the element of that index.
     generateSubSets(nums, powerSet, subSet, index + 1); // to the next index of the array we have.
     subSet.pop_back();                                  // we have removed that element so that we can not consider that and check without that element.
+
+    while (index < nums.size() - 1 && nums[index] == nums[index + 1]) // to skip all the duplicate elements, since they're already sorted.
+        index++;
+
     generateSubSets(nums, powerSet, subSet, index + 1); // the case when we moved forward and not considerd the element.
 }
 
 vector<vector<int>> subsetsWithDup(vector<int> &nums)
 {
-    set<vector<int>> powerSet;
+    sort(nums.begin(), nums.end()); // so that the duplicate elements will be contigious and easy to skip.
+
+    vector<vector<int>> powerSet;
     vector<int> subSet;
     generateSubSets(nums, powerSet, subSet, 0);
 
-    vector<vector<int>> finalSolWithoutDuplicates;
-    for (auto &it : powerSet)
-        finalSolWithoutDuplicates.push_back(it);
-
-    return finalSolWithoutDuplicates;
+    return powerSet;
 }
 };
