0fdd8b014e6f0a819b6cb2618e9e910cd765b1f0
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-09-01 19:10:42 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (59.63%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,26 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
+    /*
+        This Problem Teaches us the Use case of 
+        Mathematics in Programing,
+        In this problem the Binary Exponentiation is used, which is Better than "pow(x,n)" function in c++
+    */
+    double binaryExponentiation(double x, long long n){
+        if(n==0)
+            return 1;
+
+        if(n<0)
+            return (1.0)/binaryExponentiation(x,(-1*n));
+
+        if(n%2 == 1)
+            return x*binaryExponentiation(x*x,(n-1)/2);
+
+     return binaryExponentiation(x*x,n/2);
+
+    }
     double myPow(double x, int n) {
-        return (double)pow(x,n);
+        long long bigN = n;
+        return binaryExponentiation(x,bigN);
     }
 };
