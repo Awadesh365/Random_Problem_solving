753cba4c9d7903e8367edc36a5e4f2e488a7abdc
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-09-03 21:28:15 +0530

    Time: 4 ms (65.95%) | Memory: 9.1 MB (14.96%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
         map<int,int>mp;
         stack<int>st;
         for(int i = nums2.size()-1;i>= 0; i--){
@@ -15,9 +16,11 @@ public:
             }
             st.push(nums2[i]);
         }
+
     for(int i=0;i<nums1.size();i++){
         nums1[i] = mp[nums1[i]];
     }
+    
     return nums1;
     }
 };
