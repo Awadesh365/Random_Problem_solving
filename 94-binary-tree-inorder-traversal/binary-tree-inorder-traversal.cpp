d370848bf925dcef474289034146ec1d43577796
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-09-08 01:03:03 +0530

    Time: 4 ms (31.94%) | Memory: 8.3 MB (56.38%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,27 +1,46 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    void IO(TreeNode* root,vector<int>&ans){
-        if(root == NULL)
-            return;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-        IO(root->left,ans);
-        ans.push_back(root->val);
-        IO(root->right,ans);
-    }
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        IO(root,ans);
-        return ans;
+// /**
+//  * Definition for a binary tree node.
+//  */
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        stack<TreeNode *> st;
+        TreeNode *node = root;
+        vector<int> inOrder;
+
+        while (true)
+        {
+            if (node != NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if (st.empty())
+                {
+                    break;
+                }
+                node = st.top();
+                st.pop();
+                inOrder.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return inOrder;
     }
 };
